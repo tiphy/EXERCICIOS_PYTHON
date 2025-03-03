0a25a6533b514e9c75701efd3f07a2e315fafcb3
tiphany <111897484+tiphy@users.noreply.github.com>
2025-03-02 22:42:14 -0300

    teste

---
+++ hello.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+print(f"Nome: {nome}, Idade: {idade}, Altura: {altura:.2f}")
