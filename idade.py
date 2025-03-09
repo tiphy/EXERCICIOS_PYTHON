9d438245d36a95c0e2a21dcd1eb58ada70e76039
tiphany <111897484+tiphy@users.noreply.github.com>
2025-03-09 00:52:46 -0300

    idade

---
+++ idade.py
@@ -0,0 +1,8 @@
+idade = int(input('digite a sua idade: '))
+
+if idade >= 18:
+    print('voce é maior de idade')
+elif idade >= 12:
+    print('voce é adolecente')
+else:
+    print('voce é menor de idade')
